b7152874d46f2007047986b739e7b1beeca70d30
Giyeon Na <80277300+GiyeonNa@users.noreply.github.com>
2025-04-16 14:14:54 +0900

    가속로직 수정

--- Assets/02.Scripts/Item/SpeedBoost.cs
+++ Assets/02.Scripts/Item/SpeedBoost.cs
@@ -3,22 +3,16 @@ using UnityEngine;
 
 public class SpeedBoost : MonoBehaviour
 {
-    [SerializeField]
-    private float boostAmount = 5f; // 속도 증가량
-    [SerializeField]
-    private float boostDuration = 3f; // 속도 증가 지속 시간
+    public float boostAmount = 20f; // 속도 증가량
+    public float boostDuration = 2f; // 속도 증가 지속 시간
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        ArcadeVehicleController vehicleController = other.GetComponent<ArcadeVehicleController>();
+        if (vehicleController != null)
         {
-            Debug.Log("Player entered speed boost area");
-
-            ArcadeVehicleController vehicleController = other.GetComponent<ArcadeVehicleController>();
-            if (vehicleController != null)
-            {
-                vehicleController.ApplySpeedBoost(boostAmount, boostDuration);
-            }
+            vehicleController.ApplySpeedBoost(boostAmount, boostDuration);
+            
         }
     }
 }
